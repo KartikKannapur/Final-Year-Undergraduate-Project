6233b5614ae5717ca841261e85145643c2c3921f
Kartik Kannapur <kartikkannapur@gmail.com>
2014-03-26 16:14:35 +0530

    Update ForceSensitiveResistor.ino

--- Arduino/ForceSensitiveResistor.ino
+++ Arduino/ForceSensitiveResistor.ino
@@ -1,20 +1,38 @@
-int FSR_Pin = A0; //analog pin 0
-
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
-  int FSRReading = analogRead(FSR_Pin); 
-if(FSRReading != 0){
-  Serial.println();
-  Serial.println();
-Serial.println("Arduino");
-Serial.println();
-Serial.println();
-Serial.println();
-Serial.println();
+  int FSR_Toe = 0;
+  int FSR_Heel = 0;
+  FSR_Heel = analogRead(A0);
+  FSR_Toe = analogRead(A2);
+  
 
-  delay(250); 
-}//just here to slow down the output for easier reading
+  
+  int threshold = 300;
+
+  if (FSR_Heel > threshold){
+    FSR_Toe = 0;
+    Serial.print("FSR_Heel: ");
+    Serial.println(FSR_Heel);
+    FSR_Heel = 0;
+    FSR_Toe = 0;
+    
+    delay(500);
+   
+  }
+
+
+  if (FSR_Toe > threshold){
+    FSR_Heel = 0;
+    Serial.print("FSR_Toe: ");
+    Serial.println(FSR_Toe);
+    
+    FSR_Toe = 0;
+    FSR_Heel = 0;
+    delay(500);
+  }
+  
+
 }
